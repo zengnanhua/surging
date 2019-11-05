982e0aa8228225fcf4979d4fc15014eacf7b8590
123 <123>
2019-11-05 10:33:59 +0800

    服务名称

---
+++ src/SelfPackage/tempService/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Surging.Core.CPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tempService
+{
+    public class Startup
+    {
+        public Startup(IConfigurationBuilder config)
+        {
+            //ConfigureEventBus(config);
+            //  ConfigureCache(config);
+        }
+
+        public IContainer ConfigureServices(ContainerBuilder builder)
+        {
+            var services = new ServiceCollection();
+            //ConfigureLogging(services);
+            builder.Populate(services);
+            ServiceLocator.Current = builder.Build();
+            return ServiceLocator.Current;
+        }
+
+        public void Configure(IContainer app)
+        {
+
+        }
+    }
+}
